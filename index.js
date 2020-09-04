f080c2f4ce4f4d120ecb5f999b551d87811ae98e
codingshiksha <geekygautam1997@gmail.com>
2020-09-04 15:23:57 +0530

    second commit

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.post("/pay", (req, res) => {
       payment_method: "paypal",
     },
     redirect_urls: {
-      return_url: "http://localhost:3000/success",
-      cancel_url: "http://localhost:3000/cancel",
+      return_url: "https://paypalnode.herokuapp.com/success",
+      cancel_url: "https://paypalnode.herokuapp.com/cancel",
     },
     transactions: [
       {
